8965d7d7c11ad8e9ac831bfea3af567249ec2dd4
jb63 <jb63@sanger.ac.uk>
2025-04-01 12:09:01 +0000

    Update to filter CEGs too

--- src/figure3_manhattanplot_screenbeta.R
+++ src/figure3_manhattanplot_screenbeta.R
@@ -5,7 +5,7 @@
 library(tidyverse)
 library(ggrepel)
 library(here)
-library(biomaRt)
+# library(biomaRt)
 library(readxl)
 # Import the data from upstream steps
 # https://github.com/team113sanger/uveal_melanoma_CRISPR_downstream/blob/main/results/MAGeCK_gene_corrected_beta.tsv
@@ -22,19 +22,24 @@ fdr <- read_tsv("data/MAGeCK_gene_corrected_fdr.tsv") |>
 # Fetch Kosuke Yusa's gene ID mapping 
 download.file("https://ars.els-cdn.com/content/image/1-s2.0-S2211124716313353-mmc2.xlsx", destfile = "data/1-s2.0-S2211124716313353-mmc2.xlsx")
 hgnc_ids <- read_xlsx("data/1-s2.0-S2211124716313353-mmc2.xlsx", sheet = 2) |> 
-select(Gene, HGNC_ID) |> 
+dplyr::select(Gene, HGNC_ID) |> 
 distinct()
 
 
-hgnc_ids |> select(HGNC_ID) |> write_tsv("results/query_hgnc_ids.txt")
+hgnc_ids |> 
+select(HGNC_ID) |> 
+write_tsv("results/query_hgnc_ids.txt")
 
 # Biomart down at time of wiritng - fetch gene names from the HGNC ID manually 
 # Mart export 
 gene_id_update_map <- hgnc_ids |> 
             left_join(read_tsv("data/mart_export.txt"), by = c("HGNC_ID" = "HGNC ID")) |> 
-            select(Gene, `Gene name`) |>
+            dplyr::select(Gene, `Gene name`) |>
             distinct() |> 
             mutate(`Gene name`= case_when(is.na(`Gene name`) ~ Gene, TRUE ~ `Gene name`))
+
+gene_id_update_map |> dplyr::rename("Updated_ID" = "Gene name")
+
 
 mageck_output <- left_join(beta, fdr, by = c("genes" = "Gene", "ModelName")) |>
 dplyr::rename("Gene" = "genes") |>
@@ -43,7 +48,7 @@ select(-Gene) |>
 rename("Gene" = `Gene name`) |> 
 relocate(Gene, .before =  "ModelName")
 
-write_tsv(mageck_output, "data/mageck_output.tsv")
+write_tsv(mageck_output, "data/mageck_output_updated_ids.tsv")
 
 mageck_output_sigs_ggplot <- mageck_output |>
   mutate(
@@ -66,9 +71,17 @@ write_csv(DepMAP_24Q2_essentials |>
           as_tibble(), col_names = FALSE,
           here("results/tables/DepMAP_24Q2_essentials.csv"))
 
+
+
+hart_cegs <- read_tsv(here("data/CEGv2.txt")) |> 
+left_join(gene_id_update_map, by = c("GENE" = "Gene")) |>
+mutate(`Gene name`= case_when(is.na(`Gene name`) ~ GENE, TRUE ~ `Gene name`)) |> 
+pull(`Gene name`)
+
 # Filter mageck_output to keep genes that are not in the Essentials list
 filtered_mageck_output <- mageck_output |>
-                          filter(!(Gene %in% DepMAP_24Q2_essentials))
+                          filter(!(Gene %in% DepMAP_24Q2_essentials)) |>
+                          filter(!(Gene %in% hart_cegs))
 
 # Expand the ModelName column by separating rows, and create new rows with _NE appended
 expanded_mageck_output <- filtered_mageck_output |>
@@ -159,7 +172,7 @@ data <- combined_output |>
 
 # saveRDS(data, "data_object_for_plotting.rds")
 
-
+data
 # Plot
 set.seed(42)
 p <- ggplot(data, aes(x = ModelName, y = Beta)) +
@@ -167,11 +180,14 @@ p <- ggplot(data, aes(x = ModelName, y = Beta)) +
     position = position_jitter(0.2), stroke = NA
   ) +
   scale_color_gradient2(midpoint = 0, low = "#FF1A1A", mid = "white", high = "navyblue") +
-  geom_text_repel(aes(label = gene_name),
-    min.segment.length = 0,
-    size = 2.4,
-    max.overlaps = Inf
-  ) +
+ geom_text_repel(aes(label = gene_name),
+                min.segment.length = 0,
+                size = 2.0,
+                force = 1.1,
+                box.padding = 0.25,
+                max.iter = 10000,         # Increase iterations for better placement
+                max.overlaps = Inf
+ ) +
   labs(x = "\nModel Name", y = "Beta", shape = "Hit in > 6 models") +
   geom_hline(yintercept = c(-0.5, 0.5), col = "black", linetype = "dashed", linewidth = 0.5) +
   theme_classic() +
